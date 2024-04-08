e28c8c31220d01f448d1a228b015985c9129b3c4
June Lee <junelee@Junes-Laptop.local>
2024-04-07 19:36:54 -0500

    exercises

---
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/Planets.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+
+import java.util.ArrayList;
+public class Planets {
+
+    public static void main (String[] arg) {
+        ArrayList<String> planets = new ArrayList<>();
+
+        planets.add("Mercury");
+        planets.add("Venus");
+        planets.add("Earth");
+        planets.add("Mars");
+        planets.add("Jupiter");
+        planets.add("Saturn");
+        planets.add("Uranus");
+        planets.add("Neptune");
+    }
+}
