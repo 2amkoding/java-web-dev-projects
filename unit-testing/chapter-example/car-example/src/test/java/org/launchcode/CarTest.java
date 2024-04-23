4ec4b193bc0f2ff037e8a614092bb5ef8e0e6bfb
June Lee <junelee@Junes-Laptop.local>
2024-04-22 21:55:31 -0500

    andThisIsUnitTesting

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,11 +1,39 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+
     //TODO: constructor sets gasTankLevel properly
+    Car test_car;
+    @BeforeEach
+    public void createTestCarObject(){
+        test_car = new Car ("bmw", "M2",30, 20);
+    }
+
+    @Test
+    public void testInitialGasTank() {
+        assertEquals(50, test_car.getGasTankLevel(), .001);
+    }
+    @Test
+    public void testInitialGasTank0() {
+        assertFalse(test_car.getGasTankLevel() <= 0);
+    }
+
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void getTankLevel(){
+        test_car.drive(50);
+        assertTrue(test_car.getGasTankLevel() >= 27);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
     //TODO: can't have more gas than tank size, expect an exception
+    @Test
+    public void GasOverFill(){
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5), "Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
 }
