cbe08aa74b8c5813d003b92421057533732b32fd
June Lee <junelee@Junes-Laptop.local>
2024-05-11 21:06:44 -0500

    Completed Thymeleaf Forms

--- thymeleafViews/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ thymeleafViews/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,7 +4,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +15,15 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String eventHandler(Model model) {
-        List<String> events = new ArrayList<>();
-            events.add("Black Coffee");
-            events.add("Beyond Wonderland");
-            events.add("Fred Again..Live");
-            events.add("Boiler Room");
+//        List<String> events = new ArrayList<>();
+//            events.add("Black Coffee");
+//            events.add("Beyond Wonderland");
+//            events.add("Fred Again..Live");
+//            events.add("Boiler Room");
             model.addAttribute("events", events);
             return "events/index";
     }
@@ -27,5 +31,10 @@ public class EventController {
     @GetMapping("create") //events/create
     public String renderCreateEventForm() {
         return "events/create";
+    }
+    @PostMapping("create") //---> events/create
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 }
