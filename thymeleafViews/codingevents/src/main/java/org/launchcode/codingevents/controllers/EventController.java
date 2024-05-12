95e8dcda71d1f619028117c4d1b1b4d2b78ce655
June Lee <junelee@Junes-Laptop.local>
2024-05-11 20:19:05 -0500

    thymeLeaf Forms

---
+++ thymeleafViews/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,28 @@
+package org.launchcode.codingevents.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String eventHandler(Model model) {
+        List<String> events = new ArrayList<>();
+            events.add("Black Coffee");
+            events.add("Beyond Wonderland");
+            events.add("Fred Again..Live");
+            events.add("Boiler Room");
+            model.addAttribute("events", events);
+            return "events/index";
+    }
+
+
+}
