d251967676bd3509a4239a56761d93db2655e9e8
June Lee <junelee@Junes-Laptop.local>
2024-05-13 17:42:57 -0500

    create and render a form

---
+++ kodingevents/src/main/java/org/launchcode/kodingevents/KodingeventsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.kodingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodingeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodingeventsApplication.class, args);
+	}
+
+}
