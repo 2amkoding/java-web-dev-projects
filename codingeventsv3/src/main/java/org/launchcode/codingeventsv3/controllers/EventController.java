013178248b0566e68db4be7fc43c6ae630f53167
June Lee <junelee@Junes-Laptop.local>
2024-05-23 18:50:46 -0500

    new arraylist was overiding the global arraylist

--- codingeventsv3/src/main/java/org/launchcode/codingeventsv3/controllers/EventController.java
+++ codingeventsv3/src/main/java/org/launchcode/codingeventsv3/controllers/EventController.java
@@ -1,23 +1,39 @@
 package org.launchcode.codingeventsv3.controllers;
 
+import org.launchcode.codingeventsv3.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("events")
+@RequestMapping("/events")
 public class EventController {
+
+    private static List<Event> events = new ArrayList<>();
+
     @GetMapping
     public String handlerMethodName(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Event1");
-        events.add("Event2");
-        events.add("Event3");
+       // List<String> events = new ArrayList<>();
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+
+    @GetMapping("create")
+    public String renderEventForm() {
+        return "events/create";
+    }
+
+
+    @PostMapping("create")
+    public String createEventHandler(@RequestParam String eventName) {
+        events.add(new Event(eventName));
+        return "redirect:/events";
     }
 }
